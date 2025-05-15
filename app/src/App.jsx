c73460aa43e4d659afb293cda2b40ca547e7999c
Erik Eldridge <erikeldridge@google.com>
2025-05-15 08:54:43 -0700

    Render chat history

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -3,8 +3,9 @@ import { useEffect, useState } from "react";
 import ReactMarkdown from "react-markdown";
 
 class ChromeModel {
-  constructor(provider) {
+  constructor(provider, history = []) {
     this._provider = provider;
+    this.history = history;
   }
   async isAvailable() {
     if (!this._provider) {
@@ -24,8 +25,25 @@ class ChromeModel {
     }
   }
   async prompt(prompt) {
+    console.log(`Raw prompt: ${prompt}`);
     const session = await this.session();
-    return session.prompt(prompt);
+    const normalized = ChromeModel.normalize(prompt);
+    console.log(`Normalized prompt:`, normalized);
+    this.history.push(normalized);
+    const response = await session.prompt(normalized);
+    this.history.push([{ role: "assistant", content: response }]);
+    return response;
+  }
+  /**
+   * Normalizes to the most complex type supported by
+   * https://github.com/webmachinelearning/prompt-api#full-api-surface-in-web-idl
+   * currently sequence<LanguageModelMessageShorthand>.
+   */
+  static normalize(prompt) {
+    if (typeof prompt === "string") {
+      return [{ role: "user", content: prompt }];
+    }
+    return prompt;
   }
   async session() {
     if (!this._session) {
@@ -39,25 +57,33 @@ class ChromeModel {
 const model = new ChromeModel(window.LanguageModel);
 
 function App() {
-  const [output, setOutput] = useState("");
+  const [output, setOutput] = useState([]);
   const [isAvailable, setIsAvailable] = useState(false);
   useEffect(() => {
     model.isAvailable().then(setIsAvailable);
   }, []);
-  async function handleSubmit(e) {
+  async function onSubmit(e) {
     e.preventDefault();
     const promptEl = e.target.prompt;
-    const markdown = await model.prompt(promptEl.value);
-    setOutput(markdown);
+    await model.prompt(promptEl.value);
+    // Destructures history so React can detect the change.
+    setOutput([...model.history]);
     promptEl.value = "";
     promptEl.focus();
   }
+  const outputItems = output.flatMap((item, i) => {
+    return item.map((content, j) => {
+      return (
+        <li key={i + "-" + j} className={content.role}>
+          <ReactMarkdown children={content.content} />
+        </li>
+      );
+    });
+  });
   return (
     <div className="container">
-      <div id="output">
-        <ReactMarkdown children={output} />
-      </div>
-      <form onSubmit={handleSubmit}>
+      <ol id="output">{outputItems}</ol>
+      <form onSubmit={onSubmit}>
         <textarea name="prompt"></textarea>
         <button type="submit" disabled={!isAvailable}>
           Send
