4e4863bed348688859072627a3843b4ea52ffbb0
Erik Eldridge <erikeldridge@google.com>
2025-05-21 16:53:20 -0700

    Remove docs dir in favor of GH Action, as recommended by Vite

--- app/vite.config.js
+++ app/vite.config.js
@@ -3,9 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    // GH Pages looks for this dir.
-    outDir: '../docs'
-  }
+  plugins: [react()]
 })
